299d73a51c380f121cf48f0a8c039b23e5922279
Stiwyy <andrinrenggli@outlook.com>
2025-05-17 21:53:56 +0200

    fix all settings stuff and removed useless formatting

--- index.js
+++ index.js
@@ -10,12 +10,12 @@ const colorArray = [
     "&1", "&9", "&d", "&5",
     "&f", "&7", "&8", "&0"
 ];
-
+/*
 register("chat", bridgeChat).setCriteria("&r&2Guild > ${*} ${bot} ${*}: &r${player}" + Settings.seperator + " ${msg}&r");
 register("chat", bridgeChat).setCriteria("&r&2Guild > ${bot} ${*}: &r${player}" + Settings.seperator + " ${msg}&r");
 register("chat", bridgeChat).setCriteria("&r&2Guild > ${*} ${bot}: &r${player}" + Settings.seperator + " ${msg}&r");
 register("chat", bridgeChat).setCriteria("&r&2Guild > ${bot}: &r${player}" + Settings.seperator + " ${msg}&r");
-
+*/
 function bridgeChat(bot, player, msg, event) {
     if (!Settings.enabled || Settings.botName.toLowerCase() !== ChatLib.removeFormatting(bot).toLowerCase()) return;
     cancel(event);
@@ -73,166 +73,56 @@ function dc(input) {
 }
 
 register("chat", (...args) => {
-    let event = args[args.length - 1]; // Letztes Argument ist das Event
+    if (!Settings.enabled) return;
+    let event = args[args.length - 1];
     let decryptedLink = dc(args[2]);
-    let player = args[1]; // Der Spielername
-
-    cancel(event); // Verhindert die Anzeige der Originalnachricht
-
-    let formattedMessage = ChatLib.addColor("&2Guild > &a[VIP] MAChatbridge &6[EQUITE] &f" + player + " » " + decryptedLink);
-    
+    let player = args[1];
+    cancel(event);
+    let formattedMessage = ChatLib.addColor(`&2Guild > &a[VIP] MAChatbridge &6[EQUITE] &f${player} » ${decryptedLink}`);
     ChatLib.chat(formattedMessage);
 }).setCriteria("&r&2Guild > ${*} ${bot} ${*}: &r${player}" + Settings.seperator + " [Image] ${msg}&r");
 
+const brainRot = [
+    "skibidi", "skibidi toilet", "gyatt", "mewing", "mew", "rizz", "rizzing", "rizzler",
+    "on skibidi", "sigma", "what the sigma", "Ohio", "bussin'", "cook", "cooking",
+    "let him/her cook", "baddie", "skibidi rizz", "fanum tax", "fanum taxing", "drake",
+    "nonchalant dread head", "aura", "grimace shake", "edging", "edge", "goon", "gooning",
+    "looks maxing", "alpha", "griddy", "chris tyson", "diddy", "imagine if ninja got a low taper fade",
+    "skibidi gyatt rizz only in ohio", "duke dennis", "did you pray today", "livvy dunne rizzing up baby gronk",
+    "sussy imposter", "pibby glitch in real life", "sigma alpha omega male grindset", "andrew tate",
+    "goon cave", "freddy fazbear", "colleen ballinger", "smurf cat vs strawberry elephant", 
+    "blud dawg shmlawg", "ishowspeed", "a whole bunch of turbulence", "ambatukam", 
+    "bro really thinks he's carti", "literally hitting the griddy the ocky way", "kai cenat",
+    "fanum tax", "garten of banban", "no edging in class", "not the mosquito again", 
+    "bussing", "axel in harlem", "whopper whopper whopper whopper", "1 2 buckle my shoe",
+    "goofy ahh", "aiden ross", "sin city", "monday left me broken", 
+    "quirked up white boy busting it down sexual style", "goated with the sauce", "john pork",
+    "grimace shake", "kiki do you love me", "huggy wuggy", "nathaniel b", "lightskin stare",
+    "biggest bird", "omar the referee", "amogus", "uncanny", "wholesome reddit", "chungus",
+    "keanu reeves", "pizza tower", "zesty", "poggers", "kumalala savesta", "quandale dingle",
+    "glizzy", "rose toy", "ankha zone", "thug shaker", "morbin time", "dj khaled",
+    "sisyphus", "oceangate", "shadow wizard money gang", "ayo the pizza here", 
+    "PLUH", "nair butthole waxing", "t-pose", "ugandan knuckles", 
+    "family guy funny moments compilation with subway surfers gameplay at the bottom",
+    "nickeh30", "ratio", "uwu", "delulu", "opium bird", "cg5", "mewing",
+    "fortnite battle pass", "all my fellas", "gta 6", "backrooms", "gigachad", 
+    "based", "cringe", "kino", "redpilled", "no nut november", "pokénut november",
+    "foot fetish", "F in the chat", "i love lean", "looksmaxxing", "gassy", 
+    "social credit", "bing chilling", "xbox live", "mrbeast", "kid named finger",
+    "better caul saul", "i am a surgeon", "hit or miss i guess they never miss huh",
+    "i like ya cut g", "ice spice", "gooning", "fr", "we go gym", "kevin james",
+    "josh hutcherson", "coffin of andy and leyley", "metal pipe falling"
+];
 register("command", () => {
-    const brainRot = [
-        "skibidi",
-        "skibidi toilet",
-        "gyatt",
-        "mewing",
-        "mew",
-        "rizz",
-        "rizzing",
-        "rizzler",
-        "on skibidi",
-        "sigma",
-        "what the sigma",
-        "Ohio",
-        "bussin'",
-        "cook",
-        "cooking",
-        "let him/her cook",
-        "baddie",
-        "skibidi rizz",
-        "fanum tax",
-        "fanum taxing",
-        "drake",
-        "nonchalant dread head",
-        "aura",
-        "grimace shake",
-        "edging",
-        "edge",
-        "goon",
-        "gooning",
-        "looks maxing",
-        "alpha",
-        "griddy",
-        "chris tyson",
-        "diddy",
-        "imagine if ninja got a low taper fade",
-        "skibidi gyatt rizz only in ohio",
-        "duke dennis",
-        "did you pray today",
-        "livvy dunne rizzing up baby gronk",
-        "sussy imposter",
-        "pibby glitch in real life",
-        "sigma alpha omega male grindset",
-        "andrew tate",
-        "goon cave",
-        "freddy fazbear",
-        "colleen ballinger",
-        "smurf cat vs strawberry elephant",
-        "blud dawg shmlawg",
-        "ishowspeed",
-        "a whole bunch of turbulence",
-        "ambatukam",
-        "bro really thinks he's carti",
-        "literally hitting the griddy the ocky way",
-        "kai cenat",
-        "fanum tax",
-        "garten of banban",
-        "no edging in class",
-        "not the mosquito again",
-        "bussing",
-        "axel in harlem",
-        "whopper whopper whopper whopper",
-        "1 2 buckle my shoe",
-        "goofy ahh",
-        "aiden ross",
-        "sin city",
-        "monday left me broken",
-        "quirked up white boy busting it down sexual style",
-        "goated with the sauce",
-        "john pork",
-        "grimace shake",
-        "kiki do you love me",
-        "huggy wuggy",
-        "nathaniel b",
-        "lightskin stare",
-        "biggest bird",
-        "omar the referee",
-        "amogus",
-        "uncanny",
-        "wholesome reddit",
-        "chungus",
-        "keanu reeves",
-        "pizza tower",
-        "zesty",
-        "poggers",
-        "kumalala savesta",
-        "quandale dingle",
-        "glizzy",
-        "rose toy",
-        "ankha zone",
-        "thug shaker",
-        "morbin time",
-        "dj khaled",
-        "sisyphus",
-        "oceangate",
-        "shadow wizard money gang",
-        "ayo the pizza here",
-        "PLUH",
-        "nair butthole waxing",
-        "t-pose",
-        "ugandan knuckles",
-        "family guy funny moments compilation with subway surfers gameplay at the bottom",
-        "nickeh30",
-        "ratio",
-        "uwu",
-        "delulu",
-        "opium bird",
-        "cg5",
-        "mewing",
-        "fortnite battle pass",
-        "all my fellas",
-        "gta 6",
-        "backrooms",
-        "gigachad",
-        "based",
-        "cringe",
-        "kino",
-        "redpilled",
-        "no nut november",
-        "pokénut november",
-        "foot fetish",
-        "F in the chat",
-        "i love lean",
-        "looksmaxxing",
-        "gassy",
-        "social credit",
-        "bing chilling",
-        "xbox live",
-        "mrbeast",
-        "kid named finger",
-        "better caul saul",
-        "i am a surgeon",
-        "hit or miss i guess they never miss huh",
-        "i like ya cut g",
-        "ice spice",
-        "gooning",
-        "fr",
-        "we go gym",
-        "kevin james",
-        "josh hutcherson",
-        "coffin of andy and leyley",
-        "metal pipe falling"
-    ];
+    if (!Settings.enabled) return;
     const randomBrainrot = brainRot[Math.floor(Math.random() * brainRot.length)];
     ChatLib.chat(randomBrainrot);
-}).setName("skibidi")
+}).setName("skibidi");
 
+/*
 register("chat", function(message) {
     const randomBrainrot = brainRot[Math.floor(Math.random() * brainRot.length)];
     //Client.showTitle(randomWord, "&7", 0, 35, 15);
     ChatLib.chat(randomBrainrot);
 }).setCriteria("&r&8[&r&5392&r&8]${*}");
+*/
--- settings.js
+++ settings.js
@@ -4,9 +4,9 @@
 import { @Vigilant, @TextProperty, @ColorProperty, @ButtonProperty, @SwitchProperty, @CheckboxProperty, @SelectorProperty } from '../Vigilance/index';
 
 @Vigilant("BridgebotImages", "Settings", {
-    getCategoryComparator: () => (a, b) => {
-        const categories = ['General', 'Appearance'];
-        return categories.indexOf(a.name) - categories.indexOf(b.name);
+    getCategoryComparator: () => (a) => {
+        const categories = ['General'];
+        return categories.indexOf(a.name);
     }
 })
 class Settings {
@@ -30,83 +30,10 @@ class Settings {
         category: "General"
     })
     seperator = ":"
-    @TextProperty({
-        name: "Bridge Prefix",
-        description: "Prefix for the guild bridge",
-        category: "Appearance",
-    })
-    prefix = "[BRIDGE]"
-    @SelectorProperty({
-        name: "Bridge Color",
-        description: "Select the color of your prefix",
-        category: "Appearance",
-        options: [
-            "§4Dark Red",
-            "§cRed",
-            "§6Gold",
-         "§eYellow", 
-      "§2Dark Green", 
-      "§aGreen", 
-      "§bAqua",
-       "§3Dark Aqua", 
-       "§1Dark Blue", 
-       "§9Blue", 
-       "§dLight Purple", 
-       "§5Dark Purple", 
-       "§fWhite", 
-       "§7Gray", 
-       "§8Dark Grey", 
-       "§0Black"
-    ]
-
-    })
-    bridgeColor = 1;
-    @CheckboxProperty({
-        name: "Bridge Bold",
-        description: "Check to make the bridge prefix bold",
-        category: "Appearance",
-    })
-    prefixBold = false
-    
-    @SelectorProperty({
-        name: "Discord User Color",
-        description: "Select the color of the discord user",
-        category: "Appearance",
-        options: [
-            "§4Dark Red",
-            "§cRed",
-            "§6Gold",
-         "§eYellow", 
-      "§2Dark Green", 
-      "§aGreen", 
-      "§bAqua",
-       "§3Dark Aqua", 
-       "§1Dark Blue", 
-       "§9Blue", 
-       "§dLight Purple", 
-       "§5Dark Purple", 
-       "§fWhite", 
-       "§7Gray", 
-       "§8Dark Grey", 
-       "§0Black"
-    ]
-
-    })
-    userColor = 1;
-    
-    @CheckboxProperty({
-        name: "Discord User Bold",
-        description: "Check to make the discord user bold",
-        category: "Appearance",
-    })
-    userBold = false;
-
-
-
+   
     constructor() {
         this.initialize(this);
-        this.setCategoryDescription("General", "BetterBridge by MisterCheezeCake and camcamsatnav")
-        this.setCategoryDescription("Appearance", "BetterBridge by MisterCheezeCake and camcamsatnav")
+        this.setCategoryDescription("General", "BridgeBotImages by Stiwyy") 
     }
 }
 
