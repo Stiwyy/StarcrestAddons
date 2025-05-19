c762a124cc8a1dacdc9f0228740d2ce8dbb46070
Andrin <andrinrenggli@outlook.com>
2025-05-19 11:29:24 +0200

    added /bridge mayor command

--- commands/bridge.js
+++ commands/bridge.js
@@ -52,6 +52,31 @@ register("command", (...args) => {
             );
             break;
 
+        case "mayor":
+    request({
+        url: "https://soopy.dev/api/v2/mayor/",
+        headers: {
+            "Content-Type": "application/json",
+            "User-Agent": "Mozilla/5.0"
+        }
+    }).then(soopyResponse => {
+        const mayorData = JSON.parse(soopyResponse);
+        
+        if (!mayorData.success || !mayorData.data?.mayor) {
+            ChatLib.chat("§cCould not fetch mayor data. Try again later.");
+            return;
+        }
+
+        const mayor = mayorData.data.mayor.name;
+        const perks = mayorData.data.mayor.perks;
+        const perkNames = perks.map(perk => `§7${perk.name}`).join("\n");
+
+        ChatLib.chat(["§aCurrent Mayor: §6" + mayor, "§aPerks:", perkNames, `§7Last updated: §e${new Date(mayorData.data.lastUpdated).toLocaleString()}`].join("\n"));
+        }).catch(error => {
+            ChatLib.chat("§cError fetching mayor data: " + error);
+        });
+        break;
+
         case "commands":
         case "commandslist":
         case "help":
@@ -63,7 +88,7 @@ register("command", (...args) => {
                 "§3- /skibidi §7»" + "§8 Prints a random brainrot word." 
             ].join("\n"));
             break;
-
+        
         default:
             ChatLib.chat("§cUnknown command. Use §a/bridge commands §cfor help");
             break;
