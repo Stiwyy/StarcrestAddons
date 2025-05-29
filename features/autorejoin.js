6c5cdc8510fff939b01f3bb993ba717629fcf698
Stiwyy <andrinrenggli@outlook.com>
2025-05-29 20:04:01 +0200

    More userfriendly + easier to debug

--- features/autorejoin.js
+++ features/autorejoin.js
@@ -2,7 +2,8 @@ import Settings from "../settings";
 function autoRejoin() {
     if (!Settings.enabled && !Settings.autoRejoin) return;
     const rejoinDelay = 60000; // 1 minute
-        
+    ChatLib.chat("&aYou were kicked from Skyblock, rejoining in 1 minute...");
+    // Delay the rejoin command to prevent immediate rejoin attempts
     setTimeout(() => {
         ChatLib.command("skyblock");
         ChatLib.chat("&aRejoining Skyblock...");
